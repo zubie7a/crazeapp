df06c7ed3f9ddd797475ff29a07e0a5dd7ec412f
zubieta <santiago.zubieta@gmail.com>
2015-06-18 19:54:55 -0500

    test

--- public/js/controllers/craze.js
+++ public/js/controllers/craze.js
@@ -197,7 +197,7 @@ angular.module('starter.craze', [])
 
     setTimeout(function() {
         if(platform == 'ios' && !init) {
-            System.alert('Welcome to CraZe!', 'Scroll up this window to hide the URL bar before pressing OK. Have a great time drawing!');
+            System.alert('Welcome to CraZe!', 'Scroll up the banner to hide the URL bar. Have a great time drawing!');
             init = true;
         }
         $scope.variableInitializer();
