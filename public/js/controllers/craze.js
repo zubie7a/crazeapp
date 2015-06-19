844976639d0d7f7030165ecd97e66ff01e0ecf29
zubieta <santiago.zubieta@gmail.com>
2015-06-18 22:51:52 -0500

    test

--- public/js/controllers/craze.js
+++ public/js/controllers/craze.js
@@ -13,6 +13,7 @@ angular.module('starter.craze', [])
     // For enabling or disabling hints.
 
     platform = ionic.Platform.platform();
+    $scope.plat = platform;
     if(platform == 'android') {
         //window.location.href = "https://play.google.com/store/apps/details?id=com.zubieta.craze";        
     }
